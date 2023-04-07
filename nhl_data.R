afbeee7b796ac9e0f080107eb88ce0b3a7d552cb
jmoore6765 <j.moore6765@gmail.com>
2023-04-07 16:06:14 -0400

    Delete nhl_data.R

--- nhl_data.R
+++
@@ -1,9 +0,0 @@
-library(hockeyR)
-library(tidyverse)
-
-pbp_full = hockeyR::load_pbp(2021:2023)
-
-pbp_full = pbp_full |> 
-  filter(!is.na(xg))
-
-write.csv(pbp_full, file = "pbp_full.csv", row.names = FALSE)
