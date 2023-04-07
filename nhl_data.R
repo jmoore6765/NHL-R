d28caec7e860c4b7d2de0463355f677130164003
jmoore6765 <j.moore6765@gmail.com>
2023-04-07 09:43:13 -0400

    Update nhl_data.R

--- nhl_data.R
+++ nhl_data.R
@@ -7,6 +7,7 @@ goe_filtered = calculate_individual(pbp_full, type = "R", game_strength = "all")
   group_by(player_id) |> 
   filter(mean(goals) >= 10) |> 
   left_join(team_logos_colors, by = c("team" = "full_team_name")) |>
-  select(player_id, player_name, team_logo_espn, gp, goals, ixg, gax, team) 
+  select(player_id, player_name, team_logo_espn, gp, goals, ixg, gax, team,
+         team_color1, team_color2) 
 
 write.csv(goe_filtered, file = "goe_filtered.csv", row.names = FALSE)
