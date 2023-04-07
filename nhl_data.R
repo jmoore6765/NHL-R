20fb3b07cf5de6df19ae2a4ac743e772eefd74c5
jmoore6765 <j.moore6765@gmail.com>
2023-04-07 14:26:15 -0400

    Update nhl_data.R

--- nhl_data.R
+++ nhl_data.R
@@ -3,11 +3,7 @@ library(tidyverse)
 
 pbp_full = hockeyR::load_pbp(2021:2023)
 
-goe_filtered = calculate_individual(pbp_full, type = "R", game_strength = "all") |>
-  group_by(player_id) |> 
-  filter(mean(goals) >= 10) |> 
-  left_join(team_logos_colors, by = c("team" = "full_team_name")) |>
-  select(player_id, player_name, team_logo_espn, gp, goals, ixg, gax, team,
-         team_color1, team_color2) 
+pbp_full = pbp_full |> 
+  filter(!is.na(xg))
 
-write.csv(goe_filtered, file = "goe_filtered.csv", row.names = FALSE)
+write.csv(pbp_full, file = "pbp_full.csv", row.names = FALSE)
