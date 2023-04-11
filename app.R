1e387ff31cd30385aa10f6b3f640286111a7b5f1
jmoore6765 <j.moore6765@gmail.com>
2023-04-10 20:47:50 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-
 library(shiny)
 library(hockeyR)
 library(tidyverse)
@@ -7,13 +6,14 @@ library(gtExtras)
 library(ggplot2)
 
 # Load the play by play data from the 2021:2022 season
-pbph <- hockeyR::load_pbp(2022)
+pbp_full = hockeyR::load_pbp(2021:2023)
 
-# Looking into total expected goals and total goals over expected goals in players with over 30 goals
-goals_over_expected <- calculate_individual(pbph, type = "R", game_strength = "all") %>% 
-  group_by(player_id) %>% 
-  filter(goals >= 30) %>% 
-  left_join(team_logos_colors, by = c("team" = "full_team_name"))
+geo = calculate_individual(pbp_full, type = "R", game_strength = "all") |>
+  group_by(player_id) |> 
+  filter(mean(goals) >= 10) |> 
+  left_join(team_logos_colors, by = c("team" = "full_team_name")) |>
+  select(player_id, player_name, team_logo_espn, gp, goals, ixg, gax, team,
+         team_color1, team_color2) 
 
 # Define UI
 ui <- fluidPage(
@@ -40,10 +40,10 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-
+  
   # Render the scatter plot
   output$my_plot <- renderPlot({
-    goals_over_expected |>
+    goe |>
       na.omit() |>
       ggplot(aes(x = ixg, y = gax)) +
       geom_point(aes(fill = team_color1, color = team_color2, size = gp),
@@ -51,8 +51,8 @@ server <- function(input, output) {
       scale_color_identity(aesthetics = c("fill", "color")) +
       ggrepel::geom_text_repel(aes(label = player_name)) +
       theme_bw() +
-      geom_hline(yintercept = mean(goals_over_expected$gax), linetype = "dashed") +
-      geom_vline(xintercept = mean(goals_over_expected$ixg), linetype = "dashed") +
+      geom_hline(yintercept = mean(goe$gax), linetype = "dashed") +
+      geom_vline(xintercept = mean(goe$ixg), linetype = "dashed") +
       labs(x = "Expected Goals",
            y = "Goals Over Expected Goals",
            title = "",
@@ -65,14 +65,14 @@ server <- function(input, output) {
   
   # Render the table
   output$my_table <- render_gt({
-    goals_over_expected |> 
-      cbind(gax_rank = rank(-goals_over_expected$gax)) |> 
+    goe |> 
+      cbind(gax_rank = rank(-goe$gax)) |> 
       arrange(gax_rank) |>
       select(gax_rank, player_name, team_logo_espn, gp, goals, ixg, gax) |>
       mutate(ixg = round(ixg, 1), 
              gax = round(gax, 1)) |> 
       gt() |> 
-      tab_row_group(label = "", rows = 1:nrow(goals_over_expected)) |> 
+      tab_row_group(label = "", rows = 1:nrow(goe)) |> 
       cols_align(align = "center") |> 
       gt_img_rows(team_logo_espn) |> 
       cols_label(gax_rank = "Rank",
