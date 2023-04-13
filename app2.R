0f64da7339d0be27c8bde275e87f2f526883bc22
jmoore6765 <j.moore6765@gmail.com>
2023-04-12 20:51:52 -0400

    Update app2.R

--- app2.R
+++ app2.R
@@ -7,7 +7,6 @@ library(ggplot2)
 
 pbp_full = hockeyR::load_pbp(2022:2023)
 
-# Define UI
 ui = fluidPage(
   
   titlePanel("Goals Over Expected"),
@@ -24,30 +23,30 @@ ui = fluidPage(
     )
   )
 
-# Define server
 server = function(input, output) {
-
+  
   pbp_filtered = reactive({
     pbp_full |> 
       filter(season >= input$date_range[1] & season <= input$date_range[2])
-  })
+    
+    })
   
-  # Calculate individual statistics
   goe = reactive({
+    
     calculate_individual(pbp_filtered(), type = "R", game_strength = "all") |> 
       group_by(player_id) |> 
       filter(goals >= (input$date_range[2] - input$date_range[1]) * 30) |> 
       left_join(team_logos_colors, by = c("team" = "full_team_name")) |> 
-      select(player_id, player_name, team_logo_espn, gp, goals, ixg, gax, team,
-             team_color1, team_color2) 
-  })
+      select(player_id, player_name, team_logo_espn, gp, goals, ixg, gax, team, team_color1, team_color2) 
+    
+    })
   
   output$scatter_plot = renderPlot({
+    
     goe() |>
       na.omit() |>
       ggplot(aes(x = ixg, y = gax)) +
-      geom_point(aes(fill = team_color1, color = team_color2, size = goals),
-                 shape = 21, alpha = 0.9) +
+      geom_point(aes(fill = team_color1, color = team_color2, size = goals), shape = 21, alpha = 0.9) +
       scale_color_identity(aesthetics = c("fill", "color")) +
       ggrepel::geom_text_repel(aes(label = player_name)) +
       theme_bw() +
@@ -61,17 +60,17 @@ server = function(input, output) {
       scale_y_continuous(breaks = scales::pretty_breaks(n = 5)) +
       theme(plot.title = element_text(size = 22, hjust = 0.5, face = "bold"),
             plot.subtitle = element_text(size = 16, hjust = 0.5))
-  })
+    
+    })
   
   output$table = render_gt({
+    
     goe() |>
       cbind(gax_rank = rank(-goe()$gax)) |>
       arrange(gax_rank) |>
       select(gax_rank, player_name, team_logo_espn, gp, goals, ixg, gax) |>
-      mutate(ixg = round(ixg, 1),
-             gax = round(gax, 1)) |>
+      mutate(ixg = round(ixg, 1), gax = round(gax, 1)) |>
       gt() |>
-      style(css = "table { width: 100%; }") |> 
       tab_row_group(label = "", rows = 1:nrow(goe())) |>
       cols_align(align = "center") |>
       gt_img_rows(team_logo_espn) |> 
@@ -83,12 +82,10 @@ server = function(input, output) {
                  gp = md("Games <br> Played"), 
                  gax = md("Goals Over <br> Expected")) |>
       gt_theme_espn() |> 
-      gt_color_rows(gax, 
-                    palette = c("#E03232", "orange", "#117124"), 
-                    pal_type = "continuous")
-  })
-}
+      gt_color_rows(gax, palette = c("#E03232", "orange", "#117124"), pal_type = "continuous")
+  }, width = 800)
+  
+  }
 
-# Run the app
 shinyApp(ui = ui, server = server)
 
